59f7028f28a5cd3c5d404f13cfbd35b3c70addd2
loic <lgm.loic@gmail.com>
2020-11-15 02:12:16 +0100

    add register page integration

--- pokedex/src/Form/RegistrationFormType.php
+++ pokedex/src/Form/RegistrationFormType.php
@@ -6,6 +6,8 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,31 +19,37 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('username')
+            ->add('email', TextType::class, [
+                'attr' => ['placeholder' => 'Votre email'] 
+            ])
+            ->add('username', TextType::class, [
+                'attr' => ['placeholder' => 'Votre nom'] 
+            ])
             ->add('agreeTerms', CheckboxType::class, [
+                'attr' => ['class' => 'checkbox'],
                 'mapped' => false,
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+                    new IsTrue(),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'attr' => ['placeholder' => 'Votre mot de passe'],
                 'mapped' => false,
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
+                    new NotBlank(),
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                        'max' => 255,
                     ]),
                 ],
+            ])
+            ->add('register', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-lg btn-primary'],
+                'label' => 'S\'inscire',
             ])
         ;
     }
