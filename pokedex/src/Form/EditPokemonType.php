fd35116441210a8ab4bd8710d4f2293ff758978f
loic <lgm.loic@gmail.com>
2020-12-30 17:26:42 +0100

    add form constraints + link to admin

--- pokedex/src/Form/EditPokemonType.php
+++ pokedex/src/Form/EditPokemonType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Pokemon;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,13 +14,49 @@ class EditPokemonType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', null, ['label' => false])
-            ->add('pv', null, ['label' => false,])
-            ->add('attack', null, ['label' => false])
-            ->add('attackSpe', null, ['label' => false])
-            ->add('defense', null, ['label' => false])
-            ->add('defenseSpe', null, ['label' => false])
-            ->add('speed', null, ['label' => false])
+            ->add('name', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'minLength' => 1,
+                    'maxLength' => 50]
+            ])
+            ->add('pv', null, [
+                'label' => false,
+                'attr' => [
+                    'min' => 1,
+                    'max' => 250]
+                ])
+            ->add('attack', null, [
+                'label' => false,
+                'attr' => [
+                    'min' => 1,
+                    'max' => 250]
+                ])
+            ->add('attackSpe', null, [
+                'label' => false,
+                'attr' => [
+                    'min' => 1,
+                    'max' => 250]
+                ])
+            ->add('defense', null, [
+                'label' => false,
+                'attr' => [
+                    'min' => 1,
+                    'max' => 250]
+            ])
+            ->add('defenseSpe', null, [
+                'label' => false,
+                'attr' => [
+                    'min' => 1,
+                    'max' => 250]
+            ])
+            ->add('speed', null, [
+                'label' => false,
+                'attr' => [
+                    'min' => 1,
+                    'max' => 250]
+
+            ])
             ->add('types', null, ['label' => false])
             ->add('save', SubmitType::class, ['label' => 'Enregstrer'])
         ;
